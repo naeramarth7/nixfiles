8cd764f193be318af8552ede2c3bc0582f9b1127
Sven Herrle <sven@herrle.me>
2026-01-16 00:17:01 +0100

    fix(): file permissions for noctalia generate themes

--- home/nixos/noctalia/noctalia-shell.nix
+++ home/nixos/noctalia/noctalia-shell.nix
@@ -1,4 +1,10 @@
-{ inputs, ... }:
+{
+  pkgs,
+  inputs,
+  config,
+  lib,
+  ...
+}:
 {
   imports = [
     inputs.noctalia.homeModules.default
@@ -8,4 +14,38 @@
   programs.noctalia-shell = {
     enable = true;
   };
+
+  home.packages = with pkgs; [
+    glib # "gsettings" needed by noctalia to apply "gtk-1" themes
+  ];
+
+  # noctalia copies files from store with 444 permissions and
+  # therefore fails to update them later on. Make sure they have proper
+  # permissions by creating them and explicity changing permissions in
+  # case they were already created in a previous version.
+  home.activation.themeFiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    mkdir -p ${config.xdg.configHome}/gtk-4.0
+    touch ${config.xdg.configHome}/gtk-4.0/gtk.css
+    chmod 644 ${config.xdg.configHome}/gtk-4.0/gtk.css
+
+    mkdir -p ${config.xdg.configHome}/gtk-3.0
+    touch ${config.xdg.configHome}/gtk-3.0/gtk.css
+    chmod 644 ${config.xdg.configHome}/gtk-3.0/gtk.css
+
+    mkdir -p ${config.xdg.configHome}/qt5ct/colors
+    touch ${config.xdg.configHome}/qt5ct/colors/noctalia.conf
+    chmod 644 ${config.xdg.configHome}/qt5ct/colors/noctalia.conf
+
+    mkdir -p ${config.xdg.configHome}/qt6ct/colors
+    touch ${config.xdg.configHome}/qt6ct/colors/noctalia.conf
+    chmod 644 ${config.xdg.configHome}/qt6ct/colors/noctalia.conf
+
+    mkdir -p ${config.xdg.configHome}/spicetify/Themes/Comfy
+    touch ${config.xdg.configHome}/spicetify/Themes/Comfy/color.ini
+    chmod 644 ${config.xdg.configHome}/spicetify/Themes/Comfy/color.ini
+
+    mkdir -p ${config.xdg.configHome}/fuzzel/themes
+    touch ${config.xdg.configHome}/fuzzel/themes/noctalia
+    chmod 644 ${config.xdg.configHome}/fuzzel/themes/noctalia
+  '';
 }
