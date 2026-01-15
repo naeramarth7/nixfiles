0b223222f45e9348db50486ac0d5d9e5f32a59d8
Sven Herrle <sven@herrle.me>
2026-01-16 00:16:25 +0100

    chore(): remove nautilus

--- modules/nixos/core/applications.nix
+++ modules/nixos/core/applications.nix
@@ -1,11 +1,5 @@
 { pkgs, ... }:
 {
-  # system wide packages only
-
-  environment.systemPackages = with pkgs; [
-    nautilus
-  ];
-
   fonts = {
     fontDir.enable = true;
     packages = with pkgs; [
