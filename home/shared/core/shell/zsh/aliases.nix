c2cbef34e25083047675def042e84898d9949213
Sven Herrle <sven@herrle.me>
2026-01-27 20:10:38 +0100

    chore(zsh): add alias "ff"

--- home/shared/core/shell/zsh/aliases.nix
+++ home/shared/core/shell/zsh/aliases.nix
@@ -4,5 +4,8 @@ _: {
     ls = "ls --color=auto -h";
     reload = "exec zsh";
     watch = "watch "; # allow aliases after watch
+
+    # List all open windows and focus the selected one
+    ff = "aerospace list-windows --all | fzf | cut -d'|' -f1 | xargs aerospace focus --window-id";
   };
 }
