e52035b5438f6bb89b0e41eaf83c92f4e9c61764
Sven Herrle <sven@herrle.me>
2026-01-28 15:22:52 +0100

    chore(): remove git configuration (-> dotfiles)

--- home/shared/core/development/git/default.nix
+++ home/shared/core/development/git/default.nix
@@ -1,30 +1,7 @@
 { pkgs, ... }:
-let
-  paths = {
-    allowedSigners = ".config/git/allowed-signers";
-    config.personal = ".config/git/config.personal";
-    config.work = ".config/git/config.work";
-  };
-in
 {
   programs.git = {
     enable = true;
-
-    settings = {
-      include = {
-        path = "~/.config/git/config.main";
-      };
-    };
-  };
-
-  imports = [
-    ./gitconfig.nix
-  ];
-
-  home.file = {
-    "${paths.allowedSigners}".source = ./allowed-signers;
-    "${paths.config.personal}".source = ./gitconfig.personal;
-    "${paths.config.work}".source = ./gitconfig.work;
   };
 
   home.packages = [
