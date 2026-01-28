d095d17f48231fe5434f3ff182240051b8d4b33c
Sven Herrle <sven@herrle.me>
2026-01-28 11:05:37 +0100

    chore(sketchybar): add app icon font

--- home/darwin/core/apps/sketchybar/default.nix
+++ home/darwin/core/apps/sketchybar/default.nix
@@ -1,7 +1,11 @@
-_: {
+{ pkgs, ... }: {
   programs.sketchybar = {
     enable = true;
     service.enable = true;
     configType = "bash";
   };
+
+  home.packages = with pkgs; [
+    sketchybar-app-font
+  ];
 }
