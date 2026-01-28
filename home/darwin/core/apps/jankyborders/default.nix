185d419c41c367bd3b2bc4f71e310f48dac21813
Sven Herrle <sven@herrle.me>
2026-01-28 17:29:50 +0100

    chore(): remove borders config (-> dotfiles)

--- home/darwin/core/apps/jankyborders/default.nix
+++ home/darwin/core/apps/jankyborders/default.nix
@@ -1,12 +1,5 @@
 _: {
   services.jankyborders = {
     enable = true;
-    settings = {
-      width = 8.0;
-      active_color = "0xfff29718";
-      inactive_color = "0xff0f1419";
-      style = "round";
-      hdpi = "off";
-    };
   };
 }
