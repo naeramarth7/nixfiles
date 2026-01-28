caa02d01c35bbc013b731143a8ff0134a693a3fe
Sven Herrle <sven@herrle.me>
2026-01-28 13:07:31 +0100

    feat(aerospace): add config

--- home/darwin/core/apps/aerospace/default.nix
+++ home/darwin/core/apps/aerospace/default.nix
@@ -1,6 +1,8 @@
-_: {
+{ config, ... }: {
   programs.aerospace = {
     enable = true;
     launchd.enable = true;
   };
+
+  home.file."${config.xdg.configHome}/aerospace/aerospace.toml".source = ./aerospace.toml;
 }
