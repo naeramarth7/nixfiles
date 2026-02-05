217232fb66ae92840a883cde6d0590f5a19ab540
Sven Herrle <sven@herrle.me>
2026-02-05 22:41:06 +0100

    chore(): add packages for nvim

--- home/shared/core/neovim.nix
+++ home/shared/core/neovim.nix
@@ -1,7 +1,12 @@
-_: {
+{ pkgs, ... }: {
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
   };
+
+  home.packages = with pkgs; [
+    ripgrep # https://github.com/BurntSushi/ripgrep
+    fd # https://github.com/sharkdp/fd
+  ];
 }
