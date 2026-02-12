1946b81f89cf2ac1429ab7f1bf8902f051dafe1a
Sven Herrle <sven@herrle.me>
2026-02-12 12:10:17 +0100

    chore(): enable zramSwap

--- hosts/nixos/xsh-workstation-nix-01/hardware-configuration.nix
+++ hosts/nixos/xsh-workstation-nix-01/hardware-configuration.nix
@@ -77,7 +77,7 @@
     ];
   };
 
-  swapDevices = [ ];
+  zramSwap.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
