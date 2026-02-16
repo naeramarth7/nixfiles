c236009fd395998d9f3bf5c657e7d9810ab392ff
Sven Herrle <sven@herrle.me>
2026-02-16 12:09:23 +0100

    chore(syncthing): remove svenh-iphone-13-mini peer

--- home/shared/core/syncthing.nix
+++ home/shared/core/syncthing.nix
@@ -9,9 +9,6 @@ _: {
         "MB-HY34415F46" = {
           id = "TGOAP4U-V6SRRGP-TKRXHBB-7VJ2KUH-RPTZEBC-L53UANY-55ZPC4F-WW552QR";
         };
-        "svenh-iphone13-mini" = {
-          id = "Q4UEFWI-AEJCMOD-G5ECQZN-UNY6TFW-6GZFRQC-VW36Z5Z-PGFTRYD-FKDNPQ7";
-        };
         "svenh-pixel-10-pro" = {
           id = "S5XSQJJ-45DDZEW-VZMXWVQ-RSPWDLS-RN6G2C6-MPW3WFO-F7CPJJ5-QXE3SQU";
         };
