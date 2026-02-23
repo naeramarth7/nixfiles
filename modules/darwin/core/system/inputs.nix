bd19a787615a633bff7c7c6a7c493e24a239db88
Sven Herrle <sven@herrle.me>
2026-02-23 10:42:15 +0100

    chore(darwin): extend defaults

--- modules/darwin/core/system/inputs.nix
+++ modules/darwin/core/system/inputs.nix
@@ -6,4 +6,10 @@
   system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
   system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
   system.defaults.NSGlobalDomain.KeyRepeat = 2;
+
+  #############
+  ### Mouse ###
+  #############
+  # Drag windows via Ctrl+Cmd+Click
+  system.defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
 }
